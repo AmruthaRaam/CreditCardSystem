f62ddd0d5c2f2fd47452fb67f771a217f613e37b
AmruthaRaam <77856873+AmruthaRaam@users.noreply.github.com>
2021-01-24 16:27:44 +0530

    Update CreditCardControllerTest.java

--- CreditCardAccountProcessing/src/test/java/com/creditcard/app/controller/CreditCardControllerTest.java
+++ CreditCardAccountProcessing/src/test/java/com/creditcard/app/controller/CreditCardControllerTest.java
@@ -4,16 +4,10 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringRunner;
 
-import com.creditcard.app.CreditCardAccountProcessingApplication;
-import com.creditcard.app.controller.CreditCardController;
+
 import com.creditcard.app.dao.CreditCardRepo;
 import com.creditcard.app.model.CreditCard;
 
